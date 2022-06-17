57a3311123e44bf5f8f6f3e1e2f5ca1e354bd122
symplybennie <benedictanwa19@gmail.com>
2022-06-17 07:19:59 -0700

    update

--- src/app.py
+++ src/app.py
@@ -4,16 +4,17 @@ import os
 
 def prime(s):
     # your code goes here
-    primes = []
-    for p in range (2, n):
-	 isprime = True
-	 for num in range (2, p):
-		 if p % num == 0:
-		    isprime = False
-		    break
-	 if isPrime:
-	    prime.append(p)
-    print(primes)
+   n = int(sys.argv[1])
+   primes = []
+   for p in range (2, n):
+      isPrime = True
+      for num in range (2, p):
+         if p % num == 0:
+          isPrime = False
+          break
+      if isPrime:
+        prime.append(p)
+   print(primes)
 
 def solution(s):
     return prime(s)
